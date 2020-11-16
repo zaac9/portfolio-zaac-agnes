913f2e28ebdb9bd455c6936c19898bceca24cb34
Zaac <zaacagnes9@gmail.com>
2020-11-16 12:48:56 -0800

    Script file to hide mouse scroll animation when the user scrolls down

---
+++ script/scroll.js
@@ -0,0 +1,46 @@
+// alert();
+
+/////From: https://stackoverflow.com/questions/20632943/how-can-i-make-a-div-disappear-when-scrolling-down-100px-from-the-top
+mouseIcon = document.querySelector('.middle');
+
+$(window).bind('scroll', function(){
+	if ($(window).scrollTop() > 15) {
+		$(mouseIcon).hide();
+	} else {
+		$(mouseIcon).show();
+	}
+});
+
+
+
+// mouseIcon = document.querySelector('.middle');
+
+
+
+// window.addEventListener('scroll', function(e){
+// 	if (mouseIcon.scrollTop > 100) {
+// 		mouseIcon.style.display = "none";
+// 	} else {
+// 		mouseIcon.style.display = "block";
+// 	}
+// });
+
+// let prevScrollPos = window.pageYOffset;
+// window.onScroll = function() {
+// 	var currentScrollPos = window.pageYOffset;
+// 	if (prevScrollPos > currentScrollPos){
+// 		// mouseIcon.style.display = "none";
+// 		mouseIcon.style.top = "0";
+// 	} else {
+// 		// mouseIcon.style.display = "block";
+// 		mouseIcon.style.top = "-500px";
+// 	}
+// 	prevScrollPos = currentScrollPos;
+// }
+
+
+
+
+
+
+

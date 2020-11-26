905131f7302058ff0d78462a9df842c943d3676f
Zaac <zaacagnes9@gmail.com>
2020-11-25 23:32:12 -0800

    change color of hamburger menu when scrolling
    Also added in a seoul html page to show seoul project
    some changes to font's of the main page

--- script/scroll.js
+++ script/scroll.js
@@ -2,7 +2,14 @@
 
 /////From: https://stackoverflow.com/questions/20632943/how-can-i-make-a-div-disappear-when-scrolling-down-100px-from-the-top
 mouseIcon = document.querySelector('.middle');
-// hamburger = document.querySelector
+lines = document.querySelectorAll('.line');
+hamburger = document.querySelector('.hamburger');
+navbar = document.querySelector('.nav-bar');
+
+hamburgerHeight = hamburger.offsetHeight;
+navbarHeight = navbar.offsetHeight;
+
+// console.log("hamburger height: " + hamburgerHeight + "navbar height: " + navbarHeight)
 
 $(window).bind('scroll', function(){
 	if ($(window).scrollTop() > 100) {
@@ -11,6 +18,23 @@ $(window).bind('scroll', function(){
 		$(mouseIcon).show();
 	}
 });
+
+$(window).scroll(function() {
+	var scroll=$(window).scrollTop();
+
+	// minus 7 is to compensate for hamburger padding at top
+	if(scroll >= navbarHeight - 7 -hamburgerHeight/2) {
+		lines.forEach(function(line) {
+			line.style.background="#23B2F9";
+		});
+
+	} else {
+		lines.forEach(function(line) {
+			line.style.background="#FCFCFC";
+		});
+	}
+});
+
 
 
 

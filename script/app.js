911325b492ff5881c02246a9d23bc28599d1e8d1
Zaac <zaacagnes9@gmail.com>
2020-11-25 18:07:18 -0800

    Fixed glitches on burger menu & added media queries to nav
    Added custom code to hide burger menu on click and fixed the fade disappearing for the nav links when clicking the nav menu multiple times (in app.js)

--- script/app.js
+++ script/app.js
@@ -1,3 +1,63 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".grid-main-nav");
+let width=window.innerWidth;
+
+// if(screen.width<768) {
+hamburger.addEventListener("click", function() {
+	navLinks.classList.toggle("open");
+	console.log("clicked hamburger");
+
+	links.forEach(function(link) {
+		link.classList.toggle("fade");
+
+	// 	link.addEventListener("click", function() {
+	// 		navLinks.classList.toggle("open");
+	// 		console.log("clicked a nav-link");
+	// 		// links.forEach(function(l){
+	// 		// 	l.classList.toggle("fade");
+	// 		// });
+	// 	})
+
+	});
+});
+// }
+
+// Close Nav Menu when clicking a link
+	links.forEach(function(link) {
+		link.addEventListener("click", function() {
+			if (screen.width<768) {
+				navLinks.classList.toggle("open");
+				console.log("clicked a nav-link");
+
+				// Toglle fade class so it works next time hamburger is clicked
+				links.forEach(function(l){
+					l.classList.toggle("fade");
+				});
+			}
+		})
+
+	});
+	
+
+
+
+
+// function closeNav() {
+// 	navLinks.classList.toggle("open");
+// 	console.log("clicked a nav-link");
+// };
+
+// if (screen.width<768) {
+// 	for (var i=0; i<links.length; i++) {
+// 		links.addEventListener("click", function() {
+// 			navLinks.classList.toggle("open");
+// 			console.log("clicked a nav-link");
+// 		});
+// 	}
+// }
+
+// $(navLinks).on("click", function(){
+// 	$("menu").hide();
+// 	$("menu-icon").removeClass("active");
+// });
